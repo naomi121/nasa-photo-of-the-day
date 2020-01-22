3df97d5f2ca9defc14fe06b0e12e7fb76fa5e22d
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-01-22 18:27:54 -0500

    added nasacard import in line 3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./App.css";
+import NasaCard from "./components/NasaCard";
 
 function App() {
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        NASA PHOTO OF THE DAY 🚀!
       </p>
+      <NasaCard />
     </div>
   );
 }
